e6250d92ced108f59360eadfd602cf752cf90f52
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-05 16:47:38 +0300

    blen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
-import { useTelegram } from './components/hooks/useTelegtam';
-import Header from './components/Header/Header';
 import ProductList from './components/ProductList/ProductList';
-import Cart from './components/Cart/Cart';
 
 function App() {
-  
 
-  
   return (
 
     <div className="App">
