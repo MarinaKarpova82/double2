5c3f421ca8f36d42bfbb427f3c4842ca56584591
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-07 16:47:53 +0300

    blen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { useTelegram } from "../hooks/useTelegtam";
 import './Header.css'
 import { FaCartPlus } from "react-icons/fa6";
 import Order from "../Order";
+import './Header.css';
 
 
 /* const showOrders = (props) => {
