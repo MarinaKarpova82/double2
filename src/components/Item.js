b75de3de913b5839833d224eccbedb076d00de49
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-07 11:33:19 +0300

    blen

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,7 @@ export class Item extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={"./img/" + this.props.item.img} />
+        <img src={require(`./img/${this.props.item.img}`).default} />
         <h2>{this.props.item.title}</h2>
         <p>{this.props.item.description}</p>
         <b>{this.props.item.price}₽</b>
