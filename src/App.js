7faedda9fc81dd7285b672a16cad3d35174cbec6
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-07 11:00:37 +0300

    blen

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ class App extends React.Component {
       super(props)
       this.state = {
         products: [
-          {id: '1', img: 'nevdupl.jpg', title: 'Коть раз', price: 200, description: 'подгоревший'},
-          {id: '2', img: 'nevdupl.jpg', title: 'Коть два', price: 230, description: 'подгоревший'},
-          {id: '3', img: 'nevdupl.jpg', title: 'Коть три', price: 999, description: 'подгоревший'},
-          {id: '4', img: 'nevdupl.jpg', title: 'Коть четыри', price: 5000, description: 'подгоревший'},
-          {id: '5', img: 'nevdupl.jpg', title: 'Коть пять', price: 750, description: 'подгоревший'},
-          {id: '6', img: 'nevdupl.jpg', title: 'Коть шесть', price: 5, description: 'подгоревший'},
-          {id: '7', img: 'nevdupl.jpg', title: 'Коть семь', price: 3000, description: 'подгоревший'},
-          {id: '8', img: 'nevdupl.jpg', title: 'Коть восемь', price: 5000, description: 'подгоревший'},
+          {id: '1', img: 'cpt6.jpeg', title: 'Коть раз', price: 200, description: 'подгоревший'},
+          {id: '2', img: 'cat.jpg', title: 'Коть два', price: 230, description: 'подгоревший'},
+          {id: '3', img: 'cat2.jpg', title: 'Коть три', price: 999, description: 'подгоревший'},
+          {id: '4', img: 'cot1.jpeg', title: 'Коть четыри', price: 5000, description: 'подгоревший'},
+          {id: '5', img: 'cot2.jpeg', title: 'Коть пять', price: 750, description: 'подгоревший'},
+          {id: '6', img: 'cot3.jpeg', title: 'Коть шесть', price: 5, description: 'подгоревший'},
+          {id: '7', img: 'cot4.jpeg', title: 'Коть семь', price: 3000, description: 'подгоревший'},
+          {id: '8', img: 'cot5.jpeg', title: 'Коть восемь', price: 5000, description: 'подгоревший'},
         ]
       }
     }
