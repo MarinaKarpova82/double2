b4ea933b6ab0649bad7572a314a194b642a9b57f
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-07 16:40:24 +0300

    blen

--- src/App.js
+++ src/App.js
@@ -16,15 +16,15 @@ class App extends React.Component {
       this.state = {
         orders: [],
         products: [
-          {id: '1', img: 'cat (1).jpg', title: 'Коть раз', price: 200, description: 'подгоревший'},
-          {id: '2', img: 'cat (2).jpg', title: 'Коть два', price: 230, description: 'подгоревший'},
-          {id: '3', img: 'cat (3).jpg', title: 'Коть три', price: 999, description: 'подгоревший'},
-          {id: '4', img: 'cat (4).jpg', title: 'Коть четыри', price: 5000, description: 'подгоревший'},
-          {id: '5', img: 'cat (5).jpg', title: 'Коть пять', price: 750, description: 'подгоревший'},
-          {id: '6', img: 'cat (6).jpg', title: 'Коть шесть', price: 5, description: 'подгоревший'},
-          {id: '7', img: 'cat (7).jpg', title: 'Коть семь', price: 3000, description: 'подгоревший'},
-          {id: '8', img: 'cat (8).jpg', title: 'Коть восемь', price: 5000, description: 'подгоревший'},
-          {id: '9', img: 'cat (9).jpg', title: 'Коть восемь', price: 5000, description: 'подгоревший'},
+          {id: '1', img: 'cat (1).jpg', title: 'Коть раз', price: 200, description: 'летучий'},
+          {id: '2', img: 'cat (2).jpg', title: 'Коть два', price: 230, description: 'летучий'},
+          {id: '3', img: 'cat (3).jpg', title: 'Коть три', price: 999, description: 'летучий'},
+          {id: '4', img: 'cat (4).jpg', title: 'Коть четь', price: 5000, description: 'летучий'},
+          {id: '5', img: 'cat (5).jpg', title: 'Коть пять', price: 750, description: 'летучий'},
+          {id: '6', img: 'cat (6).jpg', title: 'Коть шесь', price: 5, description: 'летучий'},
+          {id: '7', img: 'cat (7).jpg', title: 'Коть семь', price: 3000, description: 'летучий'},
+          {id: '8', img: 'cat (8).jpg', title: 'Коть вось', price: 5000, description: 'летучий'},
+          {id: '9', img: 'cat (9).jpg', title: 'Коть девь', price: 5000, description: 'летучий'},
         ]
       }
       this.addToOrder = this.addToOrder.bind(this)
