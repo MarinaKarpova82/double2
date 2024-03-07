057b94a0711e4ae6e8433678bcbd4e2f40d85907
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-07 13:14:51 +0300

    blen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "../Button/Button";
 import { useTelegram } from "../hooks/useTelegtam";
 import './Header.css'
+import { FaCartPlus } from "react-icons/fa6";
 
 
 export default function Header() {
+
+    let [cartOpen, setCartOpen] = useState(false)
+
   return (
     <header>
         
@@ -15,6 +19,7 @@ export default function Header() {
                 <li>Кнопочк</li>
                 <li>Кнопочк</li>
             </ul>
+            <FaCartPlus onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/>
         </div>
         
     </header>
