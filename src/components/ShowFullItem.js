f20aaccfbbfefd77abf5ba0adbfd0c3389e9bf52
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-11 10:45:29 +0300

    blen

--- src/components/ShowFullItem.js
+++ src/components/ShowFullItem.js
@@ -5,6 +5,7 @@ export class ShowFullItem extends Component {
     return (
       <div className='full-item'>
       <div>
+        <img src={"./img/" + this.props.item.img} onClick={() => this.props.onShowItem(this.props.item)}/>
         <p>{this.props.item.descriptionss}</p>
       </div>
       </div>
