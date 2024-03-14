1b719b122d453cdd38bd8fd552c06d04d849c22a
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-14 10:45:28 +0300

    pis

--- src/components/base.jsx
+++ src/components/base.jsx
@@ -1,7 +1,38 @@
-import 'firebase/storage';
+import 'firebase/firestore';
 import React, { Component } from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import 'firebase/auth';
+
+
+const app = initializeApp({
+  apiKey: "AIzaSyBW8Mzad07Dil6ib3Q4NMpqQ936l0PWilw",
+  authDomain: "tg-bot-48b6a.firebaseapp.com",
+  projectId: "tg-bot-48b6a",
+  storageBucket: "tg-bot-48b6a.appspot.com",
+  messagingSenderId: "865634902520",
+  appId: "1:865634902520:web:db1fbed48ee164d16a760d",
+  measurementId: "G-68RKYV17T6"
+}
+);
+const analytics = getAnalytics(app);
 
 export class Base extends Component {
+  render() {
+    return (
+      products = [
+        { id: '1', img: 'gs://tg-bot-48b6a.appspot.com/cat (1).jpg', title: 'К', price: 200 },
+        { id: '2', img: 'gs://tg-bot-48b6a.appspot.com/cat (2).jpg', title: 'К', price: 200 }
+      ]
+      
+    )
+  }
+}
+
+export default Base
+
+
+/* export class Base extends Component {
   render() { 
   const admin = require("firebase-admin");
   const serviceAccount = require("/scr/tg-bot-8f788-firebase-adminsdk-jnsig-4fd1831197.json");
@@ -40,4 +71,4 @@ export class Base extends Component {
       </div>
     )
   }
-}}
+}} */
